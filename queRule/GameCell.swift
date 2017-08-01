5a562fdaa4e16e7975a01b1f75f3eccba8ef9dc1
winalda <alberto@kokonutstudio.com>
2017-08-01 18:25:28 -0500

    queRule
    Ejemplo de queRule

---
+++ queRule/GameCell.swift
@@ -0,0 +1,19 @@
+//
+//  GameCell.swift
+//  queRule
+//
+//  Created by Erick on 01/08/17.
+//  Copyright © 2017 Erick. All rights reserved.
+//
+
+import UIKit
+
+class GameCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblBorrowed: UILabel!
+    @IBOutlet weak var lblBorrowedTo: UILabel!
+    @IBOutlet weak var lblBorrowedDate: UILabel!
+    
+}
---
+++ queRule/GamesViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  queRule
+//
+//  Created by Erick on 01/08/17.
+//  Copyright © 2017 Erick. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class GamesViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var filterControll: UISegmentedControl!
+    
+    var managedObjectContext: NSManagedObjectContext? = nil
+    var listGames: [Game] = [Game]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
