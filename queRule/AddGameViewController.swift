7a873a6691a85044ca14290fe4dca5f51760a36e
winalda <alberto@kokonutstudio.com>
2017-08-11 18:20:42 -0500

    Avance al tutorial

--- queRule/AddGameViewController.swift
+++ queRule/AddGameViewController.swift
@@ -13,7 +13,7 @@ protocol AddGameViewControllerDelegate {
     func didAddGame()
 }
 
-class AddGameViewController: UIViewController {
+class AddGameViewController: UIViewController, UIImagePickerControllerDelegate {
 
     @IBOutlet weak var gameImageView : UIImageView!
     @IBOutlet weak var borrowedSwitch : UISwitch!
@@ -38,7 +38,90 @@ class AddGameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        dateFormatter.dateFormat = "dd/MM/yyy"
+        
+        // Keyboard -> teclado
+        
+        // Cuando se muestra el teclado (KeyBoard)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillShow), name: Notification.Name.UIKeyboardWillShow, object: nil)
+        
+        // Cuando se esconde el teclado
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardWillHide), name: Notification.Name.UIKeyboardWillHide, object: nil)
+        
+        // Se crea un gesto para que el usuario precione en la parte interior del view se esconda el teclado
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
+        // Se le asigna el gesto al view
+        self.view.addGestureRecognizer(tapGesture)
+        
+        // Se crea un gesto para cuando el usuario selecciona la imagen
+        let takePictureGesture = UITapGestureRecognizer(target: self, action: #selector(takePictureTapped))
+        // Se le asigna el gesto a la view
+        self.gameImageView.addGestureRecognizer(takePictureGesture)
 
+        imagePickerController.delegate = self
+        // allowsEditing permite hacer recortes de una imagen
+        imagePickerController.allowsEditing = true
+        
+        // El datePiker es la tabla que se muestra con todas las fechas en forma de una ruleta
+        datePicker = UIDatePicker(frame: CGRect(x: 0, y: 210, width: 320, height: 216))
+        datePicker.datePickerMode = .date
+        datePicker.addTarget(self, action: #selector(datePickerChangedValue), for: .valueChanged)
+        // Y se lo asignamos a txtBorrowedDate
+        txtBorrowedDate.inputView = datePicker
+        
+        
+        if game == nil{
+            self.title = "Añador juego"
+            self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButtonPressed))
+            self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveButtonPressed))
+            
+            self.btnDelete.isHidden = true
+            self.borrowedSwitch.isOn = false
+        }else{
+            self.title = "Detalles"
+            self.txtTitle.text = game?.title
+            if let borrowed = game?.borrowed{
+                self.borrowedSwitch.isOn = borrowed
+            }
+            
+            self.txtBorrowedTo.text = game?.borrowedTo
+            
+            if let borrowedDate = game?.borrowedDate{
+                self.txtBorrowedDate.text = dateFormatter.string(from: borrowedDate as Date)
+            }
+            
+            if let imageDate = game?.image as Data?{
+                self.gameImageView.image = UIImage(data: imageDate)
+            }
+            
+            self.btnDelete.isHidden = true
+        }
+        
+        if !borrowedSwitch.isOn{
+            txtBorrowedTo.isEnabled = false
+            txtBorrowedDate.isEnabled = false
+        }
     }
     
+    // Cuando se utilizan los metodos de take picture gesture
+    
+    func keyBoardWillShow(){}
+    
+    func keyBoardWillHide(){}
+    
+    func viewTapped(){}
+    
+    func takePictureTapped(){}
+    
+    func datePickerChangedValue(){}
+    
+    func cancelButtonPressed(){}
+    
+    func saveButtonPressed(){}
 }
+
+
+
+
+
+
